be929ba9637b0eeed5007eaf7ed43bfccb47a8ef
Yannick Luthiger <yannick.luthiger@stud.hslu.ch>
2016-12-03 16:27:46 +0100

    greetin on /start added

--- src/index.js
+++ src/index.js
@@ -1,4 +1,6 @@
 'use strict';
+var webscraper = require("./web_scraper.js");
+webscraper.scrapeThisShit("de");
 
 console.log("QoQa Telegram Bot Server");
 console.log("########################");
@@ -11,13 +13,12 @@ var token = telegramTokenReader.readFileSync('telegram_token.secret').toString()
 console.log('Secret Token: [' + token + "]");
 const bot = new TeleBot(token);
 
-bot.on('text', msg => {
+bot.on('/start', msg => {
   let fromId = msg.from.id;
   let firstName = msg.from.first_name;
   let reply = msg.message_id;
   console.log(" => respond to " + fromId);
-  return bot.sendMessage(fromId, `Welcome, ${ firstName }!`, { reply });
+  return bot.sendMessage(fromId, `Welcome ${ firstName }!`, { reply });
 });
 
 bot.connect();
-console.log("Bot is running...");
--- src/web_scraper.js
+++ src/web_scraper.js
@@ -1,22 +1,24 @@
+'use strict';
+
 const request = require('request');
 const cheerio = require('cheerio');
 
-/*
-* @param language: can be either 'de' or 'fr'. default is 'de'.
-*/
-function scrapeThisShit(language = 'de') {
-  request({
-    uri: "http://www.qoqa.ch/" + language,
-  }, function(error, response, body) {
-    var $ = cheerio.load(body);
+var webscrappermodule = {
+  /*
+  * @param language: can be either 'de' or 'fr'. default is 'de'.
+  */
+  scrapeThisShit: function(language = 'de') {
+    request({
+      uri: "http://www.qoqa.ch/" + language },
+      function(error, response, body) {
+        var $ = cheerio.load(body);
 
-    var text = $("title").text().replace('QoQa.ch | ', '');
-    console.log(text);
-    
-    var src = $('#showcase_media_DESKTOP_0').attr("src");
-    console.log(src);
-  });
+        var text = $("title").text().replace('QoQa.ch | ', '');
+        console.log(text);
+
+        var src = $('#showcase_media_DESKTOP_0').attr("src");
+        console.log(src);
+    });
+  }
 }
-
-
-scrapeThisShit();
+module.exports = webscrappermodule;
