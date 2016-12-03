72e6b5271de3269cd74c5b6e9c7017ea1a9affdf
Jan Bucher <jan@endlesscoderz.ch>
2016-12-03 14:52:09 +0100

    Add some usefull logging

--- src/index.js
+++ src/index.js
@@ -1,18 +1,23 @@
 'use strict';
 
+console.log("QoQa Telegram Bot Server");
+console.log("########################");
+
 const TeleBot = require('telebot');
 var telegramTokenReader = require('fs');
 
 // load token from file
 var token = telegramTokenReader.readFileSync('telegram_token.secret').toString().replace(/\n$/, '');
-console.log('token: ' + token);
+console.log('Secret Token: [' + token + "]");
 const bot = new TeleBot(token);
 
 bot.on('text', msg => {
   let fromId = msg.from.id;
   let firstName = msg.from.first_name;
   let reply = msg.message_id;
+  console.log(" => respond to " + fromId);
   return bot.sendMessage(fromId, `Welcome, ${ firstName }!`, { reply });
 });
 
 bot.connect();
+console.log("Bot is running...");
