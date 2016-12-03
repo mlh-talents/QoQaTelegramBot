47d216deef21b48ead2013afc9210980910b9566
Brian Boss <brianbossdennis@gmail.com>
2016-12-03 15:54:28 +0100

    Add db function

--- src/users.js
+++ src/users.js
@@ -1,9 +1,32 @@
 'use strict';
+var Datastore = require("nedb");
+var database = new Datastore({filename: 'users.db'});
+database.loadDatabase(function(err) {
+  if (err) {
+    console.error("Cannot load user database!!!");
+  }
+  else {
+    console.log("User database loaded successfull");
+  }
+});
 
-function getActiveUsers() {
+var users =  {
+  getActiveUsers: function() {
 
-}
-
-function addUser(userId, language) {
-  
-}
+  },
+  addUser: function(userId, language) {
+    var user = {
+     id: userId,
+     lang: language
+    };
+    database.insert(user, function(err, newdoc){
+      if (err) {
+        console.error("error while saving user :(");
+      }
+      else {
+        console.info("user [" + newdoc.id + "] saved into db");
+      }
+    }) ;
+  }
+};
+module.exports = users;
