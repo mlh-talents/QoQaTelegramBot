ac31f4a823ab20f7a4adfaa0116412c8356be32f
Yannick Luthiger <yannick.luthiger@stud.hslu.ch>
2016-12-03 14:37:34 +0100

    welcome message working use " ` " to interpret variables Example:` Hello ${ variable }`

---
+++ src/index.js
@@ -0,0 +1,18 @@
+'use strict';
+
+const TeleBot = require('telebot');
+var telegramTokenReader = require('fs');
+
+// load token from file
+var token = telegramTokenReader.readFileSync('telegram_token.secret');
+console.log('token: ' + token);
+const bot = new TeleBot(token.toString());
+
+bot.on('text', msg => {
+  let fromId = msg.from.id;
+  let firstName = msg.from.first_name;
+  let reply = msg.message_id;
+  return bot.sendMessage(fromId, `Welcome, ${ firstName }!`, { reply });
+});
+
+bot.connect();
